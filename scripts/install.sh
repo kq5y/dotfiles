f953c99d0072eeff9b04c66d8d330c8463f057fd
kq5y <contact@kq5.jp>
2025-06-13 23:30:36 +0900

    add: install script

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(perl -MCwd=realpath -le 'print realpath shift' "$0/..")
+
+function symlink_dir() {
+    src=$1
+    dst=$2
+    [[ -L "$dst" ]] && rm -fr "$dst"
+    ln -sf "$src" "$dst"
+}
+
+symlink_dir $SCRIPT_DIR/../.config $HOME/.config
+
+ln -sf $SCRIPT_DIR/../.zshrc $HOME/.zshrc
+ln -sf $SCRIPT_DIR/../.p10k.zsh $HOME/.p10k.zsh
+
+git clone https://github.com/kq5y/tmux-battery.git $SCRIPT_DIR/../.config/tmux/plugins/tmux-plugins/tmux-battery
+git clone https://github.com/catppuccin/tmux.git $SCRIPT_DIR/../.config/tmux/plugins/catppuccin
+
