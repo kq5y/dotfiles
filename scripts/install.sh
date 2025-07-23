2a1747456e169c8eb3b10534328aaf693e0e79a3
kq5y <contact@kq5.jp>
2025-07-23 18:59:47 +0900

    fix: script

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,6 +12,7 @@ function symlink_dir() {
 symlink_dir $SCRIPT_DIR/../.config $HOME/.config
 
 ln -sf $SCRIPT_DIR/../.zshrc $HOME/.zshrc
+ln -sf $SCRIPT_DIR/../.zprofile $HOME/.zprofile
 ln -sf $SCRIPT_DIR/../.p10k.zsh $HOME/.p10k.zsh
 
 git clone https://github.com/kq5y/tmux-battery.git $SCRIPT_DIR/../.config/tmux/plugins/tmux-plugins/tmux-battery
